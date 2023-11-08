c240c212ee4817fe0f0a9a041bf6f8a63ddbe531
Dawid Krutul <dawtul01@gmail.com>
2023-11-08 17:59:51 +0100

    tests.py for events, views first tests

--- backend/events/tests.py
+++ backend/events/tests.py
@@ -3,6 +3,15 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import Category, Event, EventNotification, EventRegistration, Comment
 
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+from datetime import datetime
+
+from .models import Event, EventNotification, EventRegistration, Category, Comment
+
 User = get_user_model()
 
 
@@ -114,3 +123,55 @@ class ModelsTests(TestCase):
         self.assertEqual(comment.user, user)
         self.assertEqual(comment.event, event)
         self.assertEqual(comment.content, "Test comment")
+
+    # Add more test cases for other view functions in EventViewSet
+
+
+class EventNotificationViewSetTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='test@example.com',
+            username='testuser',
+            password='testpassword'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_event_notification(self):
+        # Create an event first (similar to the previous test)
+        # Then create an event notification associated with that event
+        event = Event.objects.create(title='Test Event', user=self.user, description='Event description', location='Event location', is_public=True, price='25.00', capacity=100, registration_end_date=datetime.now(), start_date=datetime.now(), end_date=datetime.now())
+        url = reverse('eventnotification-list')
+        data = {
+            'event': event.id,
+            'title': 'Test Notification',
+            'content': 'Notification content',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # Add more test cases for other view functions in EventNotificationViewSet
+
+
+class CategoryViewSetTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='test@example.com',
+            username='testuser',
+            password='testpassword'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_category(self):
+        url = reverse('category-list')
+        data = {
+            'name': 'Test Category',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # Add more test cases for other view functions in CategoryViewSet
+
+
+
